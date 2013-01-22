afcce9aefd1b1f4afb1fe0a6ec26f97c22b55649
Brenton 'B-Train' Fletcher <i@bloople.net>
2013-01-22 17:37:53 +1100

    Add a script to link your dotfiles from $HOME to $HOME/key/config

---
+++ setup_dotfiles.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd ~
+rm -i .agignore && ln -nfs ~/key/config/dotfiles/.agignore
+rm -i .bash_profile && ln -nfs ~/key/config/dotfiles/.bash_profile
+rm -i .bashrc && ln -nfs ~/key/config/dotfiles/.bashrc
+rm -i .gvimrc && ln -nfs ~/key/config/dotfiles/.gvimrc
+rm -i -r .mplayer && ln -nfs ~/key/config/dotfiles/.mplayer
+rm -i -r .vim && ln -nfs ~/key/config/dotfiles/.vim
+rm -i .vimrc && ln -nfs ~/key/config/dotfiles/.vimrc
+rm -i -r .wmii-hg && ln -nfs ~/key/config/dotfiles/.wmii-hg
+rm -i .xprofile && ln -nfs ~/key/config/dotfiles/.xprofile
+
