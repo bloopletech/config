b931ea6e0cc5d983d28d3d459ded25e3dccda423
Brenton Fletcher <brentonf@jobready.com.au>
2015-08-05 13:20:43 +1000

    Rename dotfiles -> home

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,49 +1,49 @@
 #!/bin/bash
 cd ~
 rm .agignore
-ln -nfs ~/key/config/dotfiles/.agignore
+ln -nfs ~/key/config/home/.agignore
 rm .atom/config.cson
-ln -nfs ~/key/config/dotfiles/.atom/config.cson .atom/config.cson
+ln -nfs ~/key/config/home/.atom/config.cson .atom/config.cson
 rm .atom/keymap.cson
-ln -nfs ~/key/config/dotfiles/.atom/keymap.cson .atom/keymap.cson
+ln -nfs ~/key/config/home/.atom/keymap.cson .atom/keymap.cson
 rm .atom/packages.cson
-ln -nfs ~/key/config/dotfiles/.atom/packages.cson .atom/packages.cson
+ln -nfs ~/key/config/home/.atom/packages.cson .atom/packages.cson
 rm .atom/styles.less
-ln -nfs ~/key/config/dotfiles/.atom/styles.less .atom/styles.less
+ln -nfs ~/key/config/home/.atom/styles.less .atom/styles.less
 rm -r .atom/packages/fuzzy-finder
 mkdir -p .atom/packages
-ln -nfs ~/key/config/dotfiles/.atom/packages/fuzzy-finder .atom/packages/fuzzy-finder
+ln -nfs ~/key/config/home/.atom/packages/fuzzy-finder .atom/packages/fuzzy-finder
 rm .bash_profile
-ln -nfs ~/key/config/dotfiles/.bash_profile
+ln -nfs ~/key/config/home/.bash_profile
 rm .bashrc
-ln -nfs ~/key/config/dotfiles/.bashrc
+ln -nfs ~/key/config/home/.bashrc
 rm .gemrc
-ln -nfs ~/key/config/dotfiles/.gemrc
+ln -nfs ~/key/config/home/.gemrc
 rm .gvimrc
-ln -nfs ~/key/config/dotfiles/.gvimrc
+ln -nfs ~/key/config/home/.gvimrc
 rm -r .mplayer
-ln -nfs ~/key/config/dotfiles/.mplayer
+ln -nfs ~/key/config/home/.mplayer
 rm .psqlrc
-ln -nfs ~/key/config/dotfiles/.psqlrc
+ln -nfs ~/key/config/home/.psqlrc
 rm -r .vim
-ln -nfs ~/key/config/dotfiles/.vim
+ln -nfs ~/key/config/home/.vim
 rm .vimrc
-ln -nfs ~/key/config/dotfiles/.vimrc
+ln -nfs ~/key/config/home/.vimrc
 rm -r .wmii-hg
-ln -nfs ~/key/config/dotfiles/.wmii-hg
+ln -nfs ~/key/config/home/.wmii-hg
 rm .xprofile
-ln -nfs ~/key/config/dotfiles/.xprofile
+ln -nfs ~/key/config/home/.xprofile
 rm .Xresources
-ln -nfs ~/key/config/dotfiles/.Xresources
+ln -nfs ~/key/config/home/.Xresources
 rm .tmux.conf
-ln -nfs ~/key/config/dotfiles/.tmux.conf
+ln -nfs ~/key/config/home/.tmux.conf
 rm -r .i3
-ln -nfs ~/key/config/dotfiles/.i3
+ln -nfs ~/key/config/home/.i3
 rm .indicator-sysmonitor.json
-ln -nfs ~/key/config/dotfiles/.indicator-sysmonitor.json
+ln -nfs ~/key/config/home/.indicator-sysmonitor.json
 rm -r .config/roxterm.sourceforge.net
-ln -nfs ~/key/config/dotfiles/.config/roxterm.sourceforge.net .config/roxterm.sourceforge.net
+ln -nfs ~/key/config/home/.config/roxterm.sourceforge.net .config/roxterm.sourceforge.net
 rm .local/share/applications/mimeapps.list
-ln -nfs ~/key/config/dotfiles/.local/share/applications/mimeapps.list .local/share/applications/mimeapps.list
+ln -nfs ~/key/config/home/.local/share/applications/mimeapps.list .local/share/applications/mimeapps.list
 rm .local/share/applications/play.desktop
-ln -nfs ~/key/config/dotfiles/.local/share/applications/play.desktop .local/share/applications/play.desktop
+ln -nfs ~/key/config/home/.local/share/applications/play.desktop .local/share/applications/play.desktop
