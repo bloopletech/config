274bbe01ebf670941c9c49e2d79a002712eef7a0
Brenton "B-Train" Fletcher <i@bloople.net>
2026-05-06 13:38:23 +1000

    Refactor setup_dotfiles.ps1

--- setup_dotfiles.ps1
+++ setup_dotfiles.ps1
@@ -1,27 +1,50 @@
 cd "$HOME"
 
-rm .gitconfig
-New-Item -ItemType SymbolicLink -Path .gitconfig -Target C:\Repos\config\home\.gitconfig
+function rm-f ([string] $Path)
+{
+	if (Test-Path $Path)
+	{
+	    Remove-Item $Path >$null
+	}
+}
 
-rm .pulsar\config.cson
-New-Item -ItemType SymbolicLink -Path .pulsar\config.cson -Target C:\Repos\config\home\.atom\config.cson
+function ln-nfs ([string] $Target, [string] $Path)
+{
+	New-Item -ItemType SymbolicLink -Path $Path -Target $Target >$null
+}
 
-rm .pulsar\keymap.cson
-New-Item -ItemType SymbolicLink -Path .pulsar\keymap.cson -Target C:\Repos\config\home\.atom\keymap.cson
+function ln-nf ([string] $Target, [string] $Path)
+{
+	New-Item -ItemType Junction -Path $Path -Target $Target >$null
+}
 
-rm .pulsar\packages.cson
-New-Item -ItemType SymbolicLink -Path .pulsar\packages.cson -Target C:\Repos\config\home\.atom\packages.cson
+function mkdir-p ([string] $Path)
+{
+	New-Item -ItemType Directory -Force -Path $Path >$null
+}
 
-rm .pulsar\styles.less
-New-Item -ItemType SymbolicLink -Path .pulsar\styles.less -Target C:\Repos\config\home\.atom\styles.less
+rm-f .gitconfig
+ln-nfs C:\Repos\config\home\.gitconfig .gitconfig
 
-New-Item -ItemType Directory -Force -Path .pulsar\packages
-Remove-Item -Recurse .pulsar\packages\fuzzy-finder
-New-Item -ItemType Junction -Path .pulsar\packages\fuzzy-finder -Target C:\Repos\config\home\.atom\packages\fuzzy-finder
+rm-f .pulsar\config.cson
+ln-nfs C:\Repos\config\home\.atom\config.cson .pulsar\config.cson
 
-rm AppData\Roaming\ConEmu.xml
-New-Item -ItemType SymbolicLink -Path AppData\Roaming\ConEmu.xml -Target C:\Repos\config\home\AppData\Roaming\ConEmu.xml
+rm-f .pulsar\keymap.cson
+ln-nfs C:\Repos\config\home\.atom\keymap.cson .pulsar\keymap.cson
 
-New-Item -ItemType Directory -Force -Path Documents\PowerShell
-rm Documents\PowerShell\Profile.ps1
-New-Item -ItemType SymbolicLink -Path Documents\PowerShell\Profile.ps1 -Target C:\Repos\config\home\Documents\PowerShell\Profile.ps1
+rm-f .pulsar\packages.cson
+ln-nfs C:\Repos\config\home\.atom\packages.cson .pulsar\packages.cson
+
+rm-f .pulsar\styles.less
+ln-nfs C:\Repos\config\home\.atom\styles.less .pulsar\styles.less
+
+mkdir-p .pulsar\packages
+rm-f .pulsar\packages\fuzzy-finder
+ln-nf C:\Repos\config\home\.atom\packages\fuzzy-finder .pulsar\packages\fuzzy-finder
+
+rm-f AppData\Roaming\ConEmu.xml
+ln-nfs C:\Repos\config\home\AppData\Roaming\ConEmu.xml AppData\Roaming\ConEmu.xml
+
+mkdir-p Documents\PowerShell
+rm-f Documents\PowerShell\Profile.ps1
+ln-nfs C:\Repos\config\home\Documents\PowerShell\Profile.ps1 Documents\PowerShell\Profile.ps1
